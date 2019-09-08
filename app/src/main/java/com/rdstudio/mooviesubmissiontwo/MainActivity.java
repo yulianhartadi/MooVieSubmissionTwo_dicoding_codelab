b7fc6eca434cdd655b864d8058fbd3ae8bab0843
RawzaDigital <rawzadigital@gmail.com>
2019-09-08 09:09:33 +0700

    create logo actionbar, fix menu and dialog option for choosing language of app

--- app/src/main/java/com/rdstudio/mooviesubmissiontwo/MainActivity.java
+++ app/src/main/java/com/rdstudio/mooviesubmissiontwo/MainActivity.java
@@ -1,28 +1,36 @@
 package com.rdstudio.mooviesubmissiontwo;
 
+import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] LANGUAGE = new String[]{
+            "English", "Indonesia"
+    };
+    private View parent_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parent_view = findViewById(android.R.id.content);
 
         initToolbar();
         initContent();
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.main_toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setNavigationIcon(R.drawable.ic_logo);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle(null);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -42,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        changeMenuIconColor(menu, getResources().getColor(R.color.colorAccent));
+
         //Search
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
 
@@ -68,30 +76,37 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.choose_language:
-                Toast.makeText(MainActivity.this, "Selected " + item.getTitle(), Toast.LENGTH_SHORT).show();
-            case R.id.settings:
-                // Settings
-                // Choose Language menu
-                Toast.makeText(MainActivity.this, "Selected " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
-
-            default:
-                return true;
+        if (item.getItemId() == R.id.choose_language) {
+            // Choose Language menu
+            showOptionLanguage();
         }
+        return true;
     }
 
-    // color icon menu
-    public static void changeMenuIconColor(Menu menu, @ColorInt int color) {
-        for (int i = 0; i < menu.size(); i++) {
-            Drawable drawable = menu.getItem(i).getIcon();
-            if (drawable == null) continue;
-            drawable.mutate();
-            drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-        }
-    }
+    // Dialog Language Option
+    private String language_selected;
 
+    private void showOptionLanguage() {
+        language_selected = LANGUAGE[0];
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getText(R.string.choose_language));
+        builder.setSingleChoiceItems(LANGUAGE, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                language_selected = LANGUAGE[i];
+            }
+        });
+
+        builder.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar.make(parent_view, "selected : " + language_selected, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton(R.string.CANCEL, null);
+        builder.show();
+    }
 
     //Content
     private void initContent() {
